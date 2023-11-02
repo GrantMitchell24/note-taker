bfd177a75abac8dfb140d5974e5085a2b481855f
GrantMitchell24 <GrantMitchell2423@yahoo.com>
2023-11-02 15:00:37 -0500

    Changes after Gary

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/index.html'))
+res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
 
 app.get("/api/notes", (req, res) => {
@@ -24,16 +24,17 @@ app.get("/api/notes", (req, res) => {
 })
 
 app.post("/api/notes", (req, res) => {
-  let noteMOdel= {
-    title: req.body.title,
-    text: req.body.text
-  }
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    const dataObj = JSON.parse(data)
+    let noteModel= {
+      title: req.body.title,
+      text: req.body.text
+    }
+    dataObj.push(noteModel)
+    fs.writeFileSync("./db/db.json", JSON.stringify(dataObj))
+    res.json(dataObj)
+  })
 
-  database.push(noteModel)
-
-  fs.writeFileSync("./db/db.json", JSON.stringify(database))
-
-  res.json(database)
 })
 
 app.listen(PORT, () =>
